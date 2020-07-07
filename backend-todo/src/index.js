f853da6719ebadc213d012c76d3167e46de6f246
gblimoni <gabriel.limoni@raizen.com>
2020-07-07 11:03:24 -0300

    organization fix

--- backend-todo/src/index.js
+++ backend-todo/src/index.js
@@ -1,18 +1,16 @@
 require('dotenv').config({ path: '../.env' })
-
 const port = process.env.PORT || 3000
 const express = require('express')
 const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 const app = express()
-const routes = require('./routes')
 
 app.get('/', async (req, res) => res.send('Hello World'))
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use('/api/v1', routes)
 
 app.listen(port, function (e) {
