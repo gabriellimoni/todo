fe1f295a89147017228ed1131d5f22a11bf0574c
gabrielblimoni <gabrielblimoni@gmail.com>
2020-07-12 22:35:51 -0300

    inserting model to remove repository responsabilities

---
+++ backend-todo/src/models/todo.mongodb.model.js
@@ -0,0 +1,69 @@
+const Database = require('../database/index')
+const database = new Database()
+const ObjectID = require('mongodb').ObjectID
+
+class TodoMongoDbClass {
+    // for getting todos paginated and filtered,
+    // use another function
+    async getAllTodos () {
+        const db = await database.getDb()
+        const find_result = await db.collection('todos').find().toArray()
+        const todos = this._removeMongoDbUnderscoreIdFromArray(find_result)
+        return todos
+    }
+
+    async getTodoById (id) {
+        const object_id = new ObjectID(id)
+        const db = await database.getDb()
+        const find_result = await db.collection('todos').find({ _id: object_id }).toArray()
+        const todo = this._removeMongoDbUnderscoreIdFromArray(find_result)[0]
+        return todo
+    }
+
+    async addTodo (todo) {
+        const db = await database.getDb()
+        const insert_result = await db.collection('todos').insertOne(todo)
+        const new_todo = this._removeMongoDbUnderscoreIdFromArray(insert_result.ops)[0]
+        return new_todo
+    }
+
+    async deleteTodoById (id) {
+        const object_id = new ObjectID(id)
+        const db = await database.getDb()
+        await db.collection('todos').deleteOne({ _id: object_id })
+        return true
+    }
+
+    async updateTodoById (id, todo) {
+        let update_todo = await this.getTodoById(id)
+        if (update_todo) {
+            for (const key of Object.keys(todo)) {
+                if (key == 'id') continue
+                update_todo[key] = todo[key]
+            }
+            const object_id = new ObjectID(update_todo.id)
+            delete update_todo.id
+            const db = await database.getDb()
+            await db.collection('todos').updateOne(
+                {_id: object_id}, 
+                { $set: {...update_todo} }, 
+                { upsert: true }
+            )
+            const updated_todo = await this.getTodoById(id)
+            return updated_todo
+        } else {
+            return update_todo
+        }
+    }
+
+    // helper to standardize "id" instead of "_id"
+    _removeMongoDbUnderscoreIdFromArray(array) {
+        return array.map(obj => {
+            obj.id = obj._id
+            delete obj._id
+            return obj
+        })
+    }
+}
+
+module.exports = TodoMongoDbClass
--- backend-todo/src/repositories/todo.repository.js
+++ backend-todo/src/repositories/todo.repository.js
@@ -1,64 +1,29 @@
-const Database = require('../database/index')
-const database = new Database()
-const ObjectID = require('mongodb').ObjectID
+const TodoMongoDbModel = require('../models/todo.mongodb.model')
+const todoModel = new TodoMongoDbModel()
 class TodoRepository {
     async getTodos () {
-        const db = await database.getDb()
-        const find_result = await db.collection('todos').find().toArray()
-        const todos = this._removeMongoDbUnderscoreId(find_result)
+        const todos = await todoModel.getAllTodos()
         return todos
     }
 
     async getTodoById (id) {
-        const object_id = new ObjectID(id)
-        const db = await database.getDb()
-        const find_result = await db.collection('todos').find({ _id: object_id }).toArray()
-        const todo = this._removeMongoDbUnderscoreId(find_result)[0]
+        const todo = await todoModel.getTodoById(id)
         return todo
     }
 
     async addTodo (todo) {
-        const db = await database.getDb()
-        const insert_result = await db.collection('todos').insertOne(todo)
-        const new_todo = this._removeMongoDbUnderscoreId(insert_result.ops)[0]
+        const new_todo = await todoModel.addTodo(todo)
         return new_todo
     }
 
     async deleteTodoById (id) {
-        const object_id = new ObjectID(id)
-        const db = await database.getDb()
-        await db.collection('todos').deleteOne({ _id: object_id })
-        return
+        const deleted_todo = await todoModel.deleteTodoById(id)
+        return deleted_todo
     }
 
     async updateTodoById (id, todo) {
-        let update_todo = await this.getTodoById(id)
-        if (update_todo) {
-            for (const key of Object.keys(todo)) {
-                if (key == 'id') continue
-                update_todo[key] = todo[key]
-            }
-            const object_id = new ObjectID(update_todo.id)
-            delete update_todo.id
-            const db = await database.getDb()
-            await db.collection('todos').updateOne(
-                {_id: object_id}, 
-                { $set: {...update_todo} }, 
-                { upsert: true }
-            )
-            const updated_todo = await this.getTodoById(id)
-            return updated_todo
-        } else {
-            return update_todo
-        }
-    }
-
-    _removeMongoDbUnderscoreId(array) {
-        return array.map(res => {
-            res.id = res._id
-            delete res._id
-            return res
-        })
+        let updated_todo = await todoModel.updateTodoById(id, todo)
+        return updated_todo
     }
 }
 
